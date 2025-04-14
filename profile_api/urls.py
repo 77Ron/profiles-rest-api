efa6b6b3630830de37595690ff7cf77c30a380c2
Ron <ron@wymanfineart.co.uk>
2025-04-14 21:37:34 +0100

    add viewset

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 
 from profile_api import views
 
-router = DefaultRouter
-router.register('hello-viewset', views.HelloViewSet, base_name= 'hello-viewset')
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
