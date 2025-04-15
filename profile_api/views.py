d6c4ec8e156669b969b5d546a27bb255e2a0b643
Ron <ron@wymanfineart.co.uk>
2025-04-15 11:44:06 +0100

    add  get,put,patch,delete to viewset

--- profile_api/views.py
+++ profile_api/views.py
@@ -45,6 +45,8 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Test Api ViewSet"""
+    serializers_class = serializers.HelloSerializer
+
 
     def list(self, request):
         """Return Hello message."""
@@ -56,3 +58,43 @@ class HelloViewSet(viewsets.ViewSet):
         ]
         
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+    
+    def create(self, request):
+        """Create a new message."""
+
+        serializer = self.serialzer_class(data=request.data)
+       
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello (name)'
+        
+            return Response({'message': message})
+    
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST,
+                            )
+
+
+    def retrieve(self, request, PK=None):
+        """Handle getting an object by its ID."""
+
+        return Response({'http_method': 'GET'})
+    
+    def update(self, request, PK=None):
+        """Handle updatng an object."""
+
+        return Response({'http_method': 'PUT'})
+    
+    def partial_update(self, request, PK=None):
+        """Handle updating part of an object."""
+
+        return Response({'http_method': 'PATCH'})
+    
+    
+    def destroy(self, request, PK=None):
+        """Handle removing an object."""
+
+        return Response({'http_method': 'DELETE'})
+    
+    
