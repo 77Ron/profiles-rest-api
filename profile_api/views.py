ac6aa81bbea52137c0fb2ebf34fdbbc0576d8265
Ron <ron@wymanfineart.co.uk>
2025-04-15 11:53:33 +0100

    typo

--- profile_api/views.py
+++ profile_api/views.py
@@ -66,7 +66,7 @@ class HelloViewSet(viewsets.ViewSet):
        
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
-            message = f'Hello (name)'
+            message = f'Hello {name}!'
         
             return Response({'message': message})
     
