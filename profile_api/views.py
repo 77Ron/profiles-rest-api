e8902ad0abbfb51304e0fd1c6a76b8f1453d9114
Ron <ron@wymanfineart.co.uk>
2025-04-22 15:29:32 +0100

    Add viewset to new model profile feed item.

--- profile_api/views.py
+++ profile_api/views.py
@@ -114,4 +114,12 @@ class UserLoginApiView(ObtainAuthToken):
     """Handle creating use authentication tokens."""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating and updating profile feed items."""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
 
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+        serializer.save(user_profile=self.request.user)
