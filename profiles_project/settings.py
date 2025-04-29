3bbadda930bf43c11f0b78cb4b74a7d64fb37ff0
Ron <ron@wymanfineart.co.uk>
2025-04-29 13:09:51 +0100

    Add AWS config

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4g4bx$%qa7j!$^zwd5^y(yfptm$@fr0*tph2*ch3e4b08&yt7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "profile_api.UserProfile"
+
+STATIC_ROOT = 'static/'
